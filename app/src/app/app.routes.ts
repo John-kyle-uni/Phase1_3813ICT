32efe746b3fffe1ca381f4d783c81fc1b6782c96
John-Kyle-404 <jkylecadap@gmail.com>
2024-08-30 22:19:22 +1000

    Home page started

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { AccountComponent } from '../account/account.component';
+import { ChatComponent } from '../chat/chat.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component:LoginComponent},
+    {path: 'account', component:AccountComponent},
+    {path: 'chat', component:ChatComponent}];
