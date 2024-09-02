d022085f80348697f08c0c27238c231652a6d49c
John-Kyle-404 <jkylecadap@gmail.com>
2024-09-02 22:15:31 +1000

    role management

--- app/src/chat/chat.component.ts
+++ app/src/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -9,4 +10,7 @@ import { Component } from '@angular/core';
 })
 export class ChatComponent {
 
+  createGroup(){
+    alert("Hello world");
+  }
 }
